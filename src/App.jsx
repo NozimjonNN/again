84c613ff57d7ae48ea206829a284fe0fa0224ed1
Asad <Asad@gmail.com>
2024-10-31 20:38:58 +0500

    boldim

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import React from "react"
 import './global.css'
+import Faq from "./Components/Faq/Faq"
+import About from "./Components/About/About"
 import Donate from "./Components/Diyorbek/Donate"
 import Navbar from "./Components/Nozimjon/Navbar"
 import SectionContainer from "./Components/Ahmad/SectionContainer"
 
+
 const App = () => {
   return(
     <div>
+
       {/* Navbar by Nozimjon */}
       <Navbar />
       {/* DonationPage by Diyorbek + Upgraded by Nozimjon */}
       <Donate />
       <SectionContainer />
+      <About/>
+      <Faq/>
     </div>
   )
 }
